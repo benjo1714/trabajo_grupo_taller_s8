1ce5047ad146d708eea8e87490ef116464c185cc
benjo1714 <correabenjis@gmail.com>
2025-05-22 13:49:51 -0500

    main.c

--- main.c
+++ main.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include "funciones.h"
 
-// Recursos
-int tornillos, polimeros, cables, plastico, metal;
-int totalProductos = 0;
-
-// Definición de la estructura Producto
-struct Producto {
-    char nombre[50];
-    int codigo;
-    int tiempo;
-    int tornillos;
-    int polimeros;
-    int cables;
-    int plastico;
-    int metal;
-};
-
-// Arreglo para almacenar productos
-struct Producto productos[5];
-
 // Declaración de funciones
 void ingresarProductos();
 void aumentarRecursos();
@@ -33,37 +14,49 @@ int main() {
     // Entrada inicial de recursos
     PORO:
     printf("Ingrese los recursos iniciales del inventario (solo numeros positivos):\n");
-    printf("Tornillos: "); scanf("%d", &tornillos);
-    printf("Polímeros: "); scanf("%d", &polimeros);
-    printf("Cables: "); scanf("%d", &cables);
-    printf("Plástico: "); scanf("%d", &plastico);
-    printf("Metal: "); scanf("%d", &metal);
+    printf("Tornillos: ");
+    scanf("%d", &tornillos);
+    printf("Polimeros: ");
+    scanf("%d", &polimeros);
+    printf("Cables: ");
+    scanf("%d", &cables);
+    printf("Plastico: "); 
+    scanf("%d", &plastico);
+    printf("Metal: "); 
+    scanf("%d", &metal);
 
     if (tornillos < 0 || polimeros < 0 || cables < 0 || plastico < 0 || metal < 0) {
-        printf("Ingrese solo números positivos.\n");
+        printf("Ingrese solo numeros positivos.\n");
         goto PORO;
     }
 
     // Menú principal
     do {
-        printf("\n¿Qué desea hacer?\n");
+        printf("\n¿Que desea hacer?\n");
         printf("1. Ingreso de productos\n");
         printf("2. Eliminar producto\n");
         printf("3. Fabricar productos\n");
         printf("4. Aumentar recursos\n");
-        printf("5. Buscar producto por código\n");
+        printf("5. Buscar producto por codigo\n");
         printf("6. Salir\n");
-        printf("Seleccione una opción: ");
+        printf("Seleccione una opcion: ");
         scanf("%d", &opcion);
 
         switch (opcion) {
-            case 1: ingresarProductos(); break;
-            case 2: eliminarProductoPorID(); break;
-            case 3: fabricarProducto(); break;
-            case 4: aumentarRecursos(); break;
-            case 5: buscarProductoPorID(); break;
-            case 6: printf("Gracias por preferirnos.\n"); break;
-            default: printf("Opción no válida.\n"); break;
+            case 1: ingresarProductos();
+            break;
+            case 2: eliminarProductoPorID();
+            break;
+            case 3: fabricarProducto(); 
+            break;
+            case 4: aumentarRecursos();
+            break;
+            case 5: buscarProductoPorID();
+            break;
+            case 6: printf("Gracias por preferirnos.\n");
+            break;
+            default: printf("Opcion no valida.\n"); 
+            break;
         }
 
     } while (opcion != 6);
